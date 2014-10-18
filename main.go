fa1e52bc6baad998e72acc09ca4dc732d9ed8b59
Guillaume J. Charmes <guillaume@cloud.com>
2014-10-17 22:33:33 -0400

    Benchmark parallel patterns

--- main.go
+++ main.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"log"
 	"runtime"
 	"time"
 
 	"github.com/creack/goray/cli"
+	"github.com/creack/goray/objects"
 	_ "github.com/creack/goray/parser/all"
 	_ "github.com/creack/goray/render/all"
 	"github.com/creack/goray/rt"
 )
+
+// WorkRequest .
+type WorkRequest struct {
+	x, y int
+	eye  objects.Point
+	objs []objects.Object
+	fct  func(x, y int, eye objects.Point, objs []objects.Object) color.Color
+}
+
+var workerQueue chan chan WorkRequest
+
+func a(nWorkers int) {
+	// First, initialize the channel we are going to but the workers' work channels into.
+	workerQueue = make(chan chan WorkRequest, nWorkers)
+
+	// Now, create all of our workers.
+	for i := 0; i < nWorkers; i++ {
+		go func() {
+			c := make(chan WorkRequest)
+			for {
+				// Signal that we are ready for a new task
+				workerQueue <- c
+				// Wait for task dispatch
+				wr := <-c
+				wr.fct(wr.x, wr.y, wr.eye, wr.objs)
+			}
+		}()
+	}
+}
 
 func main() {
 	// Process CLI flags
@@ -29,12 +60,33 @@ func main() {
 	// Process the image
 	rtrace := rt.NewRT(sceneConf.Width, sceneConf.Height)
 	rtrace.Verbose = cliConf.Verbose
-	start := time.Now().UTC()
-	rtrace.Compute(sceneConf.Eye.Position, sceneConf.Objects)
-	fmt.Printf("%0.6fms\n", time.Since(start).Seconds()*100)
+
+	for name, fct := range map[string]func(eye objects.Point, objs []objects.Object){
+		//		"origin":         rtrace.ComputeOrigin,
+		"sema":           rtrace.ComputeSemaphone,
+		"workerDirect":   rtrace.Compute,
+		"workerIndirect": rtrace.ComputeChanResult,
+	} {
+		start := time.Now().UTC()
+		fct(sceneConf.Eye.Position, sceneConf.Objects)
+		fmt.Printf("%s:\t%0.6fms\n", name, time.Since(start).Seconds()*100)
+	}
 
 	// Render the image
 	if err := cliConf.Renderer.Renderer.Render(rtrace, sceneConf.Eye, sceneConf.Objects); err != nil {
 		log.Fatal(err)
 	}
 }
+
+/*
+	for name, fct := range map[string]func(eye objects.Point, objs []objects.Object){
+		//		"origin":         rtrace.ComputeOrigin,
+		"sema":           rtrace.ComputeSemaphone,
+		"workerDirect":   rtrace.ComputeWorkerDirect,
+		"workerIndirect": rtrace.Compute,
+	} {
+		start := time.Now().UTC()
+		fct(sceneConf.Eye.Position, sceneConf.Objects)
+		fmt.Printf("%s:\t%0.6fms\n", name, time.Since(start).Seconds()*100)
+	}
+*/
--- rt/rt.go
+++ rt/rt.go
@@ -6,6 +6,8 @@ import (
 	"image/color"
 	"runtime"
 
+	"sync"
+
 	"github.com/creack/goray/objects"
 	_ "github.com/creack/goray/objects/all"
 )
@@ -56,7 +58,103 @@ func (rt *RT) calc(x, y int, eye objects.Point, objs []objects.Object) color.Col
 	return col
 }
 
+type workQueue struct {
+	eye  objects.Point
+	objs []objects.Object
+	fct  func(x, y int, eye objects.Point, objs []objects.Object) color.Color
+	h, w int
+	c    chan int
+}
+
+type workReponse struct {
+	x, y int
+	c    color.Color
+}
+
+// Compute .
+func (rt *RT) ComputeChanResult(eye objects.Point, objs []objects.Object) {
+	rc := make(chan []workReponse, rt.Height)
+	c := make(chan int, 100)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 8; i++ {
+		go func() {
+			wg.Add(1)
+			for {
+				y, ok := <-c
+				if !ok {
+					wg.Done()
+					return
+				}
+				resp := make([]workReponse, rt.Width)
+				for x := 0; x < rt.Width; x++ {
+					if rt.Verbose && x == 0 && y%10 == 0 {
+						fmt.Printf("\rProcessing: %d%%", int((float64(y)/float64(rt.Height))*100+1))
+					}
+					resp[x].x, resp[x].y, resp[x].c = x, y, rt.calc(x, y, eye, objs)
+				}
+				rc <- resp
+			}
+		}()
+	}
+	for y := 0; y < rt.Height; y++ {
+		c <- y
+	}
+	close(c)
+	go func() {
+		wg.Wait()
+		close(rc)
+	}()
+
+	for ress := range rc {
+		for _, res := range ress {
+			rt.Img.Set(res.x, res.y, res.c)
+		}
+	}
+
+	// rt.Img.Set(x, y,
+	if rt.Verbose {
+		fmt.Printf("\rProcessing: 100%%\n")
+	}
+}
+
+// Compute .
 func (rt *RT) Compute(eye objects.Point, objs []objects.Object) {
+	c := make(chan int)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 8; i++ {
+		go func() {
+			wg.Add(1)
+			for {
+				y, ok := <-c
+				if !ok {
+					wg.Done()
+					return
+				}
+				for x := 0; x < rt.Width; x++ {
+					if rt.Verbose && x == 0 && y%10 == 0 {
+						fmt.Printf("\rProcessing: %d%%", int((float64(y)/float64(rt.Height))*100+1))
+					}
+					rt.Img.Set(x, y, rt.calc(x, y, eye, objs))
+				}
+			}
+		}()
+	}
+	for y := 0; y < rt.Height; y++ {
+		c <- y
+	}
+	// for i, total := 0, rt.Width*rt.Height; i < total; i++ {
+	// 	wq.c <- i
+	// }
+	close(c)
+	wg.Wait()
+
+	// rt.Img.Set(x, y,
+	if rt.Verbose {
+		fmt.Printf("\rProcessing: 100%%\n")
+	}
+}
+
+func (rt *RT) ComputeSemaphone(eye objects.Point, objs []objects.Object) {
 	max := runtime.NumCPU()
 	c := make(chan struct{}, max)
 	for i := 0; i < max; i++ {
@@ -78,6 +176,25 @@ func (rt *RT) Compute(eye objects.Point, objs []objects.Object) {
 		<-c
 	}
 	close(c)
+	if rt.Verbose {
+		fmt.Printf("\rProcessing: 100%%\n")
+	}
+}
+
+func (rt *RT) ComputeOrigin(eye objects.Point, objs []objects.Object) {
+	var (
+		x int
+		y int
+	)
+
+	for i, total := 0, rt.Width*rt.Height; i < total; i++ {
+		x = i % rt.Width
+		y = i / rt.Width
+		if rt.Verbose && x == 0 && y%10 == 0 {
+			fmt.Printf("\rProcessing: %d%%", int((float64(y)/float64(rt.Height))*100+1))
+		}
+		rt.Img.Set(x, y, rt.calc(x, y, eye, objs))
+	}
 	if rt.Verbose {
 		fmt.Printf("\rProcessing: 100%%\n")
 	}
